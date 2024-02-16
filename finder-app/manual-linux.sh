6e66158e13086d2211485ab5c64554b9683fdea8
Pioneer-Fringe <dingzixuan555@hotmail.com>
2024-02-15 23:20:23 -0500

    assignment-4-part-2-updates

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -96,7 +96,7 @@ sudo mknod -m 666 dev/console c 5 1
 # ZD: Clean and build the writer utility
 cd ${FINDER_APP_DIR}
 make clean
-make aarch64-none-linux-gnu-
+make ${CROSS_COMPILE}
 
 # ZD: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
